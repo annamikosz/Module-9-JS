d69158f451dd1c35cfebe5ae8f7e8bff2c153bdc
Anna Mikosz <annamikosz.lu@gmail.com>
2017-06-01 21:51:34 +0200

    Draw the tree JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var h = drawTree(3);
+
+function drawTree(h) {
+  for ( var i = 0; i < h; i++){
+    var star = '';
+    for (var j = 0; j <= i; j++){
+        star += "*";
+    }
+    console.log(star);
+  }
+}
